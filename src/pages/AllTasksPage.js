d732371d37613eee08d838528a12302d119ff144
Wineshuga <uzochukwuwinnie@gmail.com>
2024-01-30 23:40:40 +0100

    Use filterTask reducer   - Add filter buttons to page and add event listener   - Update filteredTask state using handleClick function

--- src/pages/AllTasksPage.js
+++ src/pages/AllTasksPage.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import Task from '../components/Task';
+import { filterTask } from '../redux/tasksSlice';
 
-const AllTasksPage = () => (
-  <>
-    <h2>My Tasks</h2>
-    <Task />
-  </>
-);
+const AllTasksPage = () => {
+  const dispatch = useDispatch();
+  const [filter, setFilter] = useState('');
+
+  const handleClick = (e) => {
+    const inputValue = e.target.textContent;
+    setFilter(inputValue);
+    dispatch(filterTask(inputValue));
+  };
+
+  return (
+    <>
+      <h2>My Tasks</h2>
+      <div>
+        <p>Filter</p>
+        <button type="button" value={filter} onClick={(e) => handleClick(e)}>All</button>
+        <button type="button" value={filter} onClick={(e) => handleClick(e)}>pending</button>
+        <button type="button" value={filter} onClick={(e) => handleClick(e)}>completed</button>
+      </div>
+      <Task />
+    </>
+  );
+};
 
 export default AllTasksPage;
