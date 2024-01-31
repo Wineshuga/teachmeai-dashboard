e82c9f7d8b4b9b77e79b2283f1c59080179ecb01
Wineshuga <uzochukwuwinnie@gmail.com>
2024-01-31 04:04:13 +0100

    Modify tasksSlice   - Remove all async function   - Remove all extraReducers   - Create addTask reducer to handle state update.

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -1,10 +1,4 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-
-export const getTasks = createAsyncThunk('tasks/getTasks', async () => {
-  const response = await fetch('tasks.json');
-  const data = await response.json();
-  return data.tasks;
-});
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   taskList: [],
@@ -16,26 +10,16 @@ const tasksSlice = createSlice({
   name: 'task',
   initialState,
   reducers: {
+    addTask: (state, action) => {
+      state.taskList = [...state.taskList, action.payload];
+    },
     filterTask: (state, action) => {
       const filtered = action.payload;
       const filteredTask = state.taskList.filter((task) => task.status.includes(filtered));
       state.filteredTask = filteredTask.length > 0 ? filteredTask : [];
     },
   },
-  extraReducers(builder) {
-    builder
-      .addCase(getTasks.pending, (state) => {
-        state.status = 'loading';
-      })
-      .addCase(getTasks.fulfilled, (state, { payload }) => {
-        state.taskList = payload;
-        state.status = 'success';
-      })
-      .addCase(getTasks.rejected, (state) => {
-        state.status = 'failed';
-      });
-  },
 });
 
-export const { filterTask } = tasksSlice.actions;
+export const { addTask, filterTask } = tasksSlice.actions;
 export default tasksSlice.reducer;
