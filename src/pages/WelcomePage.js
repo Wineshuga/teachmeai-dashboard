0a7f9ea8cc7260bc3c5e93701bb9b9c01cbd3ff2
Wineshuga <uzochukwuwinnie@gmail.com>
2024-01-31 15:57:24 +0100

    Style responsive dashboard

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Outlet } from 'react-router';
 import Dashboard from '../components/Dashboard';
 
-const WelcomePage = () => (
-  <section>
-    <Dashboard />
-    <section>
-      <Outlet />
+const WelcomePage = () => {
+  const [dashboardVisible, setDashboardVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setDashboardVisible((prevVisibility) => !prevVisibility);
+  };
+
+  return (
+    <section className="flex">
+      <button type="button" onClick={toggleVisibility} className="md:hidden absolute py-3 mx-2 text-xl">
+        {' '}
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+      <section className={`md:block ${dashboardVisible ? 'visible' : 'hidden'} absolute bg-sky-500 pt-12 p-3 h-screen`}>
+        <Dashboard visible={toggleVisibility} />
+      </section>
+      <section className="md:pt-3 px-2 pt-10">
+        <Outlet />
+      </section>
     </section>
-  </section>
-);
+  );
+};
 
 export default WelcomePage;
