2fb51375ca3dcf40de0c23c5fce6f5323d8c42c9
Wineshuga <uzochukwuwinnie@gmail.com>
2024-01-31 04:07:28 +0100

    Remove useEffect and dispatch

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,16 +1,8 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getTasks } from '../redux/tasksSlice';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Task = () => {
-  const dispatch = useDispatch();
   const { filteredTask, taskList } = useSelector((store) => store.task);
-
-  useEffect(() => {
-    if (taskList.length === 0) {
-      dispatch(getTasks());
-    }
-  }, [taskList.length, dispatch]);
 
   return (
     <>
