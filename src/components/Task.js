1c8932b6505363ad2dc74ad33fe96d7cb2eb90a9
Wineshuga <uzochukwuwinnie@gmail.com>
2024-01-31 04:17:26 +0100

    Add conditional rendering of text when no tasks is found

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Task = () => {
   const { filteredTask, taskList } = useSelector((store) => store.task);
 
   return (
     <>
+      { (filteredTask.length === 0 && taskList.length === 0) && (
+        <div>
+          <p>You have no tasks on the list</p>
+          <Link to="../add-task">Create a New Task</Link>
+        </div>
+      ) }
+
       { (filteredTask.length > 0 ? filteredTask : taskList).map((item) => (
         <div key={item.id}>
           <span>{item.status}</span>
